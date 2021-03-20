91715446e0f7b95eceba5399b1e5680d71285f39
Karo Lencina <karolencina@Karolinas-Air.home>
2021-03-20 15:54:12 +0100

    a functional React.js project Envelope from Pure React by Dave Ceddia.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var sender = {
+    name: "Mr Sender",
+    street: "123 Fake St.",
+    city: "Boston"
+  };
+
+var receiver = {
+    name: "Mr Receiver",
+    street: "123 Fake St",
+    city: "Boston"
+  };
+
+const Envelope = ({ fromPerson, toPerson }) => {
+  return (
+    <div className="content">
+      <div className="sender">
+        <AddressLabel nameAL={fromPerson.name} streetAL={fromPerson.street} cityAL={fromPerson.city}/>
+      </div>
+      <div>
+        <Stamp />
+      </div>
+      <div className="receiver">
+        <AddressLabel nameAL={toPerson.name} streetAL={toPerson.street} cityAL={toPerson.city}/>
+      </div>
+    </div>
+  )
+};
+
+const AddressLabel = ({ nameAL, streetAL, cityAL }) => {
+  return (
+    <span className="address-label">
+      <div>{nameAL}</div>
+      <div>{streetAL}</div>
+      <div>{cityAL}</div>
+    </span>
+  )
+};
+
+const Stamp = () => {return (<div className="stamp"></div>)}
+
+ReactDOM.render(<Envelope fromPerson={sender} toPerson={receiver}/>, document.getElementById('root'));
